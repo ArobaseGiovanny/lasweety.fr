92a2c6d123c70ef1cc9986dd8955d55cf437af1b
ArobaseGiovanny <arobasegiovanny@gmail.com>
2025-11-14 22:54:06 +0100

    error text fix

--- backend/routes/checkout.js
+++ backend/routes/checkout.js
@@ -10,7 +10,7 @@ import { PACKAGING, selectPackaging } from "../config/shipping.js";
 import { generateInvoicePdfBuffer } from "../services/invoicePdf.js";
 import fs from "fs";
 import path from "path";
-import { appendOrderToSheet } from "../services/googleSheets.js";
+import { appendOrderToSheet } from "../services/googlesheets.js";
 
 
 dotenv.config();
