655c6657a7cf3a46667c272154790cda91e354e5
ArobaseGiovanny <arobasegiovanny@gmail.com>
2025-11-20 17:37:00 +0100

    change img out of stock blue

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -2,15 +2,13 @@ import { useRef, useEffect } from "react";
 import "./hero.scss";
 import sweetyxOrange from "../../assets/icons/sweetyx-orange.png";
 import sweetyxMarron from "../../assets/icons/sweetyx-marron-rupturedestock.png";
-import sweetyxBleu from "../../assets/icons/sweetyx-bleu.png";
+import sweetyxBleu from "../../assets/icons/sweetyx-bleu-rupturedestock.png";
 import sweetyxRose from "../../assets/icons/sweetyx-rose-rupturedestock.png";
 import tableBois from "../../assets/background/table.jpg";
 import { IoIosArrowDown } from "react-icons/io";
 import "../../scripts/adaptWidth";
 
-
 function Hero({ onPelucheClick }) {
-
   const scrollRef = useRef(null);
 
   useEffect(() => {
@@ -19,7 +17,7 @@ function Hero({ onPelucheClick }) {
 
     const onWheel = (e) => {
       if (e.deltaY !== 0) {
-        e.preventDefault();             
+        e.preventDefault();
         el.scrollLeft += e.deltaY * 2;
       }
     };
@@ -31,16 +29,32 @@ function Hero({ onPelucheClick }) {
 
   return (
     <main className="hero" ref={scrollRef}>
-        <div className="hero__text">
-            <span className="hero__text-choose">Choisis ta peluche</span>
-            <span className="hero__text-choose">Sweetyx</span>
-            <IoIosArrowDown />
-        </div>
+      <div className="hero__text">
+        <span className="hero__text-choose">Choisis ta peluche</span>
+        <span className="hero__text-choose">Sweetyx</span>
+        <IoIosArrowDown />
+      </div>
       <div className="hero__images">
-        <img src={sweetyxOrange} alt="Ourson de couleur orange" onClick={() => onPelucheClick(101)}/>
-        <img src={sweetyxBleu} alt="Ourson de couleur bleu" onClick={() => onPelucheClick(102)}/>
-        <img src={sweetyxRose} alt="Ourson de couleur rose" onClick={() => onPelucheClick(103)}/>
-        <img src={sweetyxMarron} alt="Ourson de couleur marron" onClick={() => onPelucheClick(104)}/>
+        <img
+          src={sweetyxOrange}
+          alt="Ourson de couleur orange"
+          onClick={() => onPelucheClick(101)}
+        />
+        <img
+          src={sweetyxBleu}
+          alt="Ourson de couleur bleu"
+          onClick={() => onPelucheClick(102)}
+        />
+        <img
+          src={sweetyxRose}
+          alt="Ourson de couleur rose"
+          onClick={() => onPelucheClick(103)}
+        />
+        <img
+          src={sweetyxMarron}
+          alt="Ourson de couleur marron"
+          onClick={() => onPelucheClick(104)}
+        />
       </div>
       <img src={tableBois} alt="Table en bois" className="hero__table" />
     </main>
